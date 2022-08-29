75bde0f26b1f3b042ca8cf810d445b3d974dcc6e
Mehmet Doganay <mehmetdoganay@Mehmet-MacBook-Air.local>
2022-08-29 18:13:02 +0300

    my solution

---
+++ HackerRank Java Stdin and Stdout I/Main.java
@@ -0,0 +1,20 @@
+// challenge link : https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem?isFullScreen=false
+import java.io.*;
+import java.util.*;
+import  java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner (System.in);
+
+
+        for (int i = 0; i < 3; i++)
+        {
+            int num = input.nextInt();
+            System.out.println(num);
+        }
+
+
+    }
+}
